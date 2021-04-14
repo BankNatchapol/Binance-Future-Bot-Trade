0e3477d1d8bf5300b2cf5296d8f79f0f23ecfb05
BankNatchapol <natchapol.pat@gmail.com>
2021-04-14 18:58:49 +0700

    feat: frame add left and right frame into root
    Resolves:
    See also:

--- interface/root_component.py
+++ interface/root_component.py
@@ -1,5 +1,15 @@
 import tkinter as tk
+from interface.styling import *
 
 class Root(tk.Tk):
     def __init__(self):
         super().__init__()
+        self.title("Trading Bot")
+
+        self.configure(bg = BG_COLOR)
+
+        self.left_frame = tk.Frame(self, bg = BG_COLOR)
+        self.left_frame.pack(side = tk.LEFT)
+        
+        self.right_frame = tk.Frame(self, bg = BG_COLOR)
+        self.right_frame.pack(side = tk.RIGHT)
