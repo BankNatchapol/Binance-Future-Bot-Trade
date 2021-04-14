1ba6a6d05e4f93c222c192b26872b7d7a71a8c37
BankNatchapol <natchapol.pat@gmail.com>
2021-04-14 19:41:24 +0700

    feat: logging module add logging module
    Resolves:
    See also:

--- interface/root_component.py
+++ interface/root_component.py
@@ -1,5 +1,8 @@
 import tkinter as tk
+import time 
+
 from interface.styling import *
+from interface.logging_component import Logging
 
 class Root(tk.Tk):
     def __init__(self):
@@ -13,3 +16,10 @@ class Root(tk.Tk):
         
         self.right_frame = tk.Frame(self, bg = BG_COLOR)
         self.right_frame.pack(side = tk.RIGHT)
+
+        self._logging_frame = Logging(self.left_frame, bg = BG_COLOR)
+        self._logging_frame.pack(side = tk.TOP)
+
+        self._logging_frame.add_log("This is test message.")
+        time.sleep(2)
+        self._logging_frame.add_log("This is another test message.")
