270d430fc909863bd148b6b606ddf287550392c1
BankNatchapol <natchapol.pat@gmail.com>
2021-04-21 10:00:19 +0700

    feat: database workspace add database workspace module
    Resolves:
    See also:

---
+++ interface/database.py
@@ -0,0 +1,33 @@
+import sqlite3
+import typing 
+
+class WorkspaceData:
+    def __init__(self):
+        self.conn = sqlite3.connect("database.db")
+        self.conn.row_factory = sqlite3.Row
+        self.cursor = self.conn.cursor()
+        
+        self.cursor.execute("CREATE TABLE IF NOT EXISTS watchlist (symbol TEXT, exchange TEXT)")
+        self.cursor.execute("CREATE TABLE IF NOT EXISTS strategies (strategy_type TEXT, contract TEXT," 
+                            "timeframe TEXT, balance_pct REAL, take_profit REAL, stop_loss REAL, extra_params TEXT)")
+        
+        self.comm.commit()
+
+    def save(self, table: str, data: typing.List[typing.Tuple]):
+        self.cursor.execure(f"DELETE FROM {table}")
+
+        table_data = self.cursor.execute("SELECT * FROM {table}")
+        
+        columns = [description[0] for description in table_data.description]
+
+        sql_statement = f"INSERT INTO {table} ({', '.join(columns)}) VALUES ({', '.join(['?'] * len(columns))})"
+
+        self.cursor.executemany(sql_statement, data)
+        self.conn.commit()
+
+
+    def get(self, table: str) -> typing.List[sqlite3.Row]:
+        self.cursor.execute(f"SELECT * FROM {table}")
+        data = self.cursor.fetchall()
+        
+        return data
