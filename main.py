53ec2181cb7c208103701761389df25b7e86ca55
BankNatchapol <natchapol.pat@gmail.com>
2021-04-21 15:16:45 +0700

    fix: change key variable

--- main.py
+++ main.py
@@ -29,7 +29,7 @@ logger.addHandler(stream_handler)
 
 if __name__ == "__main__":
 
-    binance = BinanceClient(BINANCE_SPOT_PUBLIC_KEY, BINANCE_SPOT_SECRET_KEY, testnet = True, futures = True)
+    binance = BinanceClient(BINANCE_TESTNET_FUTURE_PUBLIC_KEY, BINANCE_TESTNET_FUTURE_SECRET_KEY, testnet = True, futures = True)
     
     root = Root(binance)
 
